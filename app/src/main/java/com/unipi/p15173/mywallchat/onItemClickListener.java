4e35c0f46635348f73387fa6159c9fc4d96b3351
Buffito <thodoriskotoufos@hotmail.com>
2025-03-05 21:55:43 +0200

    Started update to bring it up to date.

--- app/src/main/java/com/unipi/p15173/mywallchat/onItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.unipi.p15173.mywallchat;
-
-import android.view.View;
-
-//interface for viewholder (recycler view)
-public interface onItemClickListener {
-
-    public void onClick(View view, int index);
-}
